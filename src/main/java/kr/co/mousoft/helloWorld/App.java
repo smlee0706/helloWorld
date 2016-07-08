bc5d950d89507f397af762099e2c0b6dc622081c
Samsung <Samsung@User-pc>
2016-07-08 17:41:15 +0900

    1st commit

---
+++ src/main/java/kr/co/mousoft/helloWorld/App.java
@@ -0,0 +1,13 @@
+package kr.co.mousoft.helloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
