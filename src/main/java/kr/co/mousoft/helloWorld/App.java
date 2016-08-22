1da072a4e9e78eb62d6654dba6880d665bc9ae3c
Samsung <Samsung@Seungmin>
2016-08-22 16:31:16 +0900

    1st commit

--- src/main/java/kr/co/mousoft/helloWorld/App.java
+++ src/main/java/kr/co/mousoft/helloWorld/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello Seungmin!" );
+        System.out.println( "1st Commit & Pish!")
     }
 }
