f4ebad7ce1f1ea98c3e9335a936a302e675ef2c9
Samsung <Samsung@Seungmin>
2016-08-23 10:17:18 +0900

    BAN-11 #assign issue close

--- src/main/java/kr/co/mousoft/helloWorld/App.java
+++ src/main/java/kr/co/mousoft/helloWorld/App.java
@@ -20,5 +20,6 @@ public class App
         System.out.println( "After email match with jira user");
         System.out.println( "no idea");
         System.out.println( "BAN-11 issue close");
+
     }
 }
